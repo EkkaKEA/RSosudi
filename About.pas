132aa433c10ec44d3fc1bb9bdc6281f03ee6dd62
EkkaKEA <canabis220@mail.ru>
2022-07-10 20:57:14 +0300

    Create About.pas

---
+++ About.pas
@@ -0,0 +1,33 @@
+unit About;
+
+interface
+
+uses Windows, SysUtils, Classes, Graphics, Forms, Controls, StdCtrls,
+  Buttons, ExtCtrls;
+
+type
+  TAboutBox = class(TForm)
+    OKButton: TButton;
+    Panel2: TPanel;
+    Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  AboutBox: TAboutBox;
+
+implementation
+
+{$R *.DFM}
+
+end.
+ 
